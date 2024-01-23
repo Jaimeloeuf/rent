bf6ac07185f32a54c284ab5c5ac442564070798b
Jaime <jaimeloeuf@gmail.com>
2024-01-23 20:01:34 +0800

    Refactor App and Main.tsx layout and handle errors: 1.  Refactor App and Main.tsx UI layouts to move all UI layering into     App.tsx as the root UI element and leave main.tsx only with the main     config and entry point. 2.  Add react-error-boundary dependency to catch and handle errors in     App.tsx instead of crashing the whole application.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import { Suspense } from 'react';
+import { StrictMode, Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 import { RouterProvider } from 'react-router-dom';
+import './App.css';
 
-import { Loading } from './components/Loading';
+import { Loading } from './components';
 import { router } from './router';
+
+function GlobalError() {
+  return <div>Something went wrong</div>;
+}
 
 export function App() {
   return (
-    <div className="p-4 sm:p-6">
-      <Suspense fallback={<Loading />}>
-        <RouterProvider router={router} />
-      </Suspense>
-    </div>
+    <StrictMode>
+      <ErrorBoundary fallback={<GlobalError />}>
+        <Suspense fallback={<Loading />}>
+          <div className="p-4 sm:p-6">
+            <RouterProvider router={router} />
+          </div>
+        </Suspense>
+      </ErrorBoundary>
+    </StrictMode>
   );
 }
